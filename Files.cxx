8e1fd5569a1a865a0b86648215d3a27fefa3610b
Vivien Mallet <Vivien.Mallet@inria.fr>
2004-03-16 01:00:16 +0000

    Added class 'ConfigStream'.

--- Files.cxx
+++ Files.cxx
@@ -31,7 +31,7 @@ namespace Talos
     \return true if the file exists, false otherwise.
     \note This function returns false if the user has not the rights to
     read the file, even if the file exists.
-   */
+  */
   bool exists(string file_name)
   {
     ifstream file_stream(file_name.c_str(), ifstream::in);
@@ -39,6 +39,115 @@ namespace Talos
     file_stream.close();
     
     return ans;
+  }
+
+
+  //////////////////
+  // CONFIGSTREAM //
+  //////////////////
+
+  //! Default constructor.
+  /*! Nothing is performed.
+   */
+  ConfigStream::ConfigStream():
+    comments_("#%"), delimiters_(" \t")
+  {
+  }
+
+  //! Main constructor.
+  /*! Opens a file.
+    \param file_name file to be opened.
+  */
+  ConfigStream::ConfigStream(string file_name,
+			     string comments,
+			     string delimiters):
+    ifstream(file_name.c_str()), file_name_(file_name),
+    comments_(comments), delimiters_(delimiters)
+  {
+  }
+
+  //! Checks whether a line should be discarded.
+  /*!
+    \param line line to be checked.
+  */
+  bool ConfigStream::Discard(string line) const
+  {
+    size_t first = line.find_first_not_of(delimiters_);
+    return ( (first == string::npos)
+	     || (comments_.find_first_of(line[first]) != string::npos) );
+  }
+
+  //! Sets the characters considered as delimiters..
+  /*!
+    \param delimiters delimiters.
+  */
+  void ConfigStream::SetDelimiters(string delimiters)
+  {
+    delimiters_ = delimiters;
+  }
+
+  //! Sets the characters that denote a comment line.
+  /*!
+    \param comments the characters that denote a comment line.
+  */
+  void ConfigStream::SetComments(string comments)
+  {
+    comments_ = comments;
+  }
+
+  //! Returns the characters considered as delimiters..
+  /*!
+    \return Delimiters.
+  */
+  string ConfigStream::GetDelimiters() const
+  {
+    return delimiters_;
+  }
+
+  //! Returns the characters that denote a comment line.
+  /*!
+    \return The characters that denote a comment line.
+  */
+  string ConfigStream::GetComments() const
+  {
+    return comments_;
+  }
+
+  //! Returns the name of the file that was opened.
+  /*!
+    \return The name of the file that was opened.
+  */
+  string ConfigStream::GetFileName() const
+  {
+    return file_name_;
+  }
+
+  //! Opens a file.
+  /*!
+    \param file_name file name.
+    \param mode (optional) flags describing the requested I/O mode for the file.
+    Default: in.
+    \note If a file was previously opened, it is closed and the stream is cleared.
+  */
+  void ConfigStream::Open(string file_name, openmode mode)
+  {
+    this->close();
+    this->clear();
+    this->open(file_name.c_str(), mode);
+
+    file_name_ = file_name;
+  }
+
+  //! Closes the current file.
+  /*!
+    \note The stream is cleared.
+  */
+  void ConfigStream::Close()
+  {
+    this->close();
+    this->clear();
+
+    file_name_ = "";
   }
 
 }  // namespace Talos.
--- Files.hxx
+++ Files.hxx
@@ -26,6 +26,35 @@ namespace Talos
 
   bool exists(string file_name);
 
+  class ConfigStream: public ifstream
+  {
+  private:
+    //! File name associated with the stream.
+    string file_name_;
+    //! Characters that denote a comment line.
+    string comments_;
+    //! Characters considered as delimiters.
+    string delimiters_;
+
+  public:
+    ConfigStream();
+    ConfigStream(string file_name,
+		 string comments = "#%",
+		 string delimiters = " \t");
+
+    bool Discard(string line) const;
+
+    void SetDelimiters(string delimiters);
+    void SetComments(string comments);
+
+    string GetDelimiters() const;
+    string GetComments() const;
+    string GetFileName() const;
+
+    void Open(string file_name, openmode mode = in);
+    void Close();
+  };
+
 }  // namespace Talos.
 
 
