421964ab62b0fb71fdd131b29263c4fff855e818
Meryem Ahmed de Biasi <Meryem.Ahmed_de_Biasi@inria.fr>
2007-10-01 09:09:59 +0000

    Updated copyright and header.

--- String.hxx
+++ String.hxx
@@ -1,21 +1,20 @@
-// Copyright (C) 2004-2005 Vivien Mallet
+// Copyright (C) 2004-2007, Vivien Mallet
 //
-// This file is part of Talos library.
-// Talos library provides miscellaneous tools to make up for C++
-// lacks and to ease C++ programming.
+// This file is part of Talos library, which provides miscellaneous tools to
+// make up for C++ lacks and to ease C++ programming.
 //
-// Talos is free software; you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation; either version 2 of the License, or
-// (at your option) any later version.
+// Talos is free software; you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the Free Software
+// Foundation; either version 2 of the License, or (at your option) any later
+// version.
 //
-// Talos is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License (file "license") for more details.
+// Talos is distributed in the hope that it will be useful, but WITHOUT ANY
+// WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+// FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+// details.
 //
-// For more information, please see the Talos home page:
-//     http://spacetown.free.fr/lib/talos/
+// For more information, visit the Talos home page:
+//     http://vivienmallet.net/lib/talos/
 
 
 #ifndef TALOS_FILE_STRING_HXX
